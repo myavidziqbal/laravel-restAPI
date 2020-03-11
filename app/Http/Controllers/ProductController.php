4528b73b17592174ad14fa52c275be5651f3ee7e
myavidziqbal <sademark08@gmail.com>
2020-03-11 15:28:32 +0700

    Product Collection

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 
+use App\Http\Resources\Product\ProductCollection;
 use App\Http\Resources\Product\ProductResource;
 use App\Model\Product;
 use Illuminate\Http\Request;
@@ -17,7 +18,7 @@ class ProductController extends Controller
     public function index()
     {
         //
-        return Product::all();
+        return ProductCollection::collection(Product::all());
     }
 
     /**
